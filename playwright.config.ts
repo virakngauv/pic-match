5d01ab705c4ac8d393aa1a7defe621e39fa43d98
Virak Ngauv <virakngauv@Viraks-MacBook-Pro.local>
2026-08-09 19:48:41 -0700

    Add complete multiplayer race coverage

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, devices } from '@playwright/test'
 
-const baseURL = 'http://127.0.0.1:3100'
+const baseURL = process.env.PLAYWRIGHT_BASE_URL ?? 'http://127.0.0.1:3100'
 
 export default defineConfig({
   testDir: './e2e',
